51ed7e35719c9116939e1a1d72c48a94f52992bb
zongshi89811 <yy89811@hotmail.com>
2013-10-16 00:48:45 -0500

    failed function, need fix later

--- read_data_function.py
+++ read_data_function.py
@@ -31,35 +31,6 @@ def read_data_function(start_date,end_date,site_no):
     discharge = np.array(discharge)
     dates = np.array(dates)
 
-    discharge = discharge * 0.0283
+#    discharge = discharge * 0.0283
     
-data_1 = read_data_function(start_date,end_date,site_no)
-
-mean  = []
-std   = [] 
-dates = data_1[1]
-discharge = data_1[0]
-
-month = np.array([d.month for d in dates])
-day   = np.array([d.day for d in dates])
     
-for idx in dates:
-    cal_discharge = discharge [(month ==idx.month) & (day ==idx.day)]
-    mean.append (np.mean (cal_discharge))
-    std.append (np.std (cal_discharge))
-
-mean = np.array(mean)
-std  = np.array(std)
-
-upper = mean + std
-lower = mean - std
-
-plot_year = np.array([d.year for d in dates])
-idx       = np.where (plot_year >= 2005 & plot_year >= 2007)
-plot_dates    = dates[idx]
-plot_discharge = discharge [idx]
-plot_mean      = mean[idx]
-plot_upper     = plot_mean+ std[idx]
-plot_lower   = plot_mean+ std[idx]
-
-plot_function(plot_year,plot_dates,plot_discharge,plot_mean,plot_upper,plot_lower)
